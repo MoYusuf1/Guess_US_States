27374758ea8f8ce3e5b9a2286ed776731bbb75e8
Your Name <you@example.com>
2022-06-02 11:26:06 -0500

    Polished the game fully and added a lot of new features. I will be moving on to the next python project!
    New Features:
    * added a feature that accumulates all the missed states into a file for the user to learn from
    * added a tally in the title of the window prompt that shows the user how many states they guess
    * added a hidden quit feature that you access by typing quit mid-game

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ screen.addshape(image)
 turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+
 guessed_states = []
 
+
 while len(guessed_states) < 50:
+    answered = len(guessed_states)
+    answer_state = screen.textinput(title=f"{answered}/50 States", prompt="Name a state").title()
 
-    answer_state = screen.textinput(title="Guess the State", prompt="Name a state").title()
-
-    if answer_state == "Quit":
+    if answer_state == "Exit":
         break
 
     if answer_state in all_states:
@@ -26,12 +28,28 @@ while len(guessed_states) < 50:
         state_data = data[data["state"] == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
-        guessed_states.append(t)
+        guessed_states.append(answer_state)
     else:
-        choice = screen.textinput(title="WRONG", prompt="Wrong! You probably misspelled...\n\n Would you like to start "
-                                                        "over? \n\n Yes or No?")
+        # I did wrote the prompt that way so it would center properly.
+        choice = screen.textinput(title="WRONG", prompt="Would you like to start over? \n         "
+                                                        "       Yes or No").title()
         if choice == "Yes":
-            guessed_states = []
+            guessed_states.clear()
         elif choice == "No":
-            pass
+            break
 
+# collects the missed states from the for loop below
+missed_states = []
+
+for state in all_states:
+    if state in guessed_states:
+        pass
+    else:
+        missed_states.append(state)
+
+# converts the list into a dictionary
+missed_states_dict = {"Missed States": missed_states}
+# converts that dictionary to a pandas dataframe
+missed_states_df = pandas.DataFrame(missed_states_dict)
+# converts that data frame to a csv file
+missed_states_df.to_csv("states_missed.csv")
